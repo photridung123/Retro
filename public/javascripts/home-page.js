28266e697ace6ef83d9514cf85e0813a8ade3dae
photridung123 <55821539+photridung123@users.noreply.github.com>
2020-11-17 21:43:03 +0700

    - update feedback layout

--- public/javascripts/home-page.js
+++ public/javascripts/home-page.js
@@ -1,7 +1,44 @@
-$(document).ready(function() {
-    //Set the carousel options
-    $('.carousel').carousel({
-      pause: true,
-      interval: 4000,
-    });
+$(document).ready(function () {
+
+  //init
+  let width;
+  let max_card;
+  let head = 1;
+  init_card();
+
+
+  $('.btn-prev').click(function () {
+    if (head > 1) {
+
+      $('.quote-' + (head + max_card - 1).toString()).hide();
+      head--;
+      $('.quote-' + head.toString()).show();
+    }
   });
+
+  $('.btn-next').click(function () {
+    if (head < 5 - max_card) {
+      $('.quote-' + head.toString()).hide();
+      head++;
+      $('.quote-' + (head + max_card - 1).toString()).show();
+    }
+  });
+
+  $(window).resize(function(){
+    init_card();
+  });
+
+  function init_card() {
+
+    console.log("checked");
+    width = $(window).width();
+    max_card = (width > 995) ? 3 : 1;
+    $('.quote').hide();
+
+    if (max_card == 3) {
+      for (let i = head; i < head + max_card; i++) {
+        $('.quote-' + i.toString()).show();
+      }
+    } else $('.quote-' + head.toString()).show();
+  }
+});
