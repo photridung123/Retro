91b21fc74120d4ce55fa0a329acab92efda6214e
ninonesix <mninonesix@gmail.com>
2020-12-05 23:27:58 +0700

    + xong giao diện page team ( chờ duyệt của nhóm trưởng )

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -3,11 +3,33 @@ exports.index = (req, res, next) => {
     
     // Pass data to view to display
     res.render('team',
-    team=
     {
-        total_member: 1, 
-        name: "our board", 
-        owner: 1, 
-        id: 1}
-    );
+        team: 
+        [
+            {
+                owner           : "Vo Van Ba Dat",
+                name            : "Dteam",
+                member:
+                [
+                    {
+                        name: "Pho Tri Dung",
+                    },
+                    {
+                        name: "Nguyen Tri Dung",
+                    }
+                ]
+            },
+            {
+                owner           : "Vo Nguc Duc",
+                name            : "teamD",
+                member:
+                [
+                    {
+                        name: "Nguyen Phu Duy",
+                    }
+                ]
+            }
+        ],
+        layout: 'dashboard/main', title: "Dashboard", ID: 1
+    })
 };
--- public/javascripts/team.js
+++ public/javascripts/team.js
@@ -1,9 +1,36 @@
 $("document").ready(function () {
-    data = {};
-    $("#okBtn").click(function () {
-        data = $("#teamname").val();
-        console.log(data);
-        $('#createTeamModal').modal('hide');
-        $("#teamname").val('');
+    // teamname = {}; // lưu team name khi tạo team mới
+    // $("#okBtn").click(function () {
+    //     teamname = $("#teamname").val();
+    //     $('#createTeamModal').modal('hide');
+    //     $("#teamname").val('');
+    // })
+    let current = 4;
+    $(".memberIncresement").click(function(){
+        let my = "<div class=\"pt-2\"><input type=\"text\" class=\"myteam-input\" id=\"member"+ current.toString() +"\" placeholder=\"Member's email\"></div>";
+        $(".input-boxes").append(my);
+        ++current;
+    })
+
+    $(".inviteBtn").click(function(){
+        $(".input-boxes").html("");
+        current = 1;
+        while(current<4)
+        {
+            my = "<div class=\"pt-2\"><input type=\"text\" class=\"myteam-input\" id=\"member"+ current.toString() +"\" placeholder=\"Member's email\"></div>";
+            $(".input-boxes").append(my);
+            ++current;
+        }
+    })
+    $("#addMoreMemberModal").on("hidden.bs.modal", function () {
+        $(".input-boxes").html("");
+        current = 1;
+        while(current<4)
+        {
+            my = "<div class=\"pt-2\"><input type=\"text\" class=\"myteam-input\" id=\"member"+ current.toString() +"\" placeholder=\"Member's email\"></div>";
+            $(".input-boxes").append(my);
+            ++current;
+        }
     })
 })
+
