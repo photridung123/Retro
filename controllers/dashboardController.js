3661a05f0dced7aba1ac85a4afd151009f74d78d
photridung123 <55821539+photridung123@users.noreply.github.com>
2020-12-05 21:59:41 +0700

    + update dashboard view

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,7 +4,6 @@ exports.index = (req, res, next) => {
     // Pass data to view to display
     res.render('vwdashboard',
         {
-            total_public_boards: 2,
             public_boards: [
                 {
                     boardID: 1,
@@ -26,32 +25,33 @@ exports.index = (req, res, next) => {
                     teamID: 1,
                     teamName: 'TeamD',
                     owner: 1,
-                    total_team_boards: 1,
+                    team_boards: [
+                        
+                        {
+                            boardID: 3,
+                            teamID: 1,
+                            boardName: 'Project abc',
+                            date_created: new Date(),
+                            total_card: 4
+                        }
+                    ]
                 },
                 {
                     teamID: 2,
                     teamName: 'DTeam',
                     owner: 2,
-                    total_team_boards: 1,
+                    team_boards: [
+                
+                        {
+                            boardID: 4,
+                            teamID: 2,
+                            boardName: 'Project Web',
+                            date_created: new Date(),
+                            total_card: 2
+                        }
+                    ]
                 }
-            ],
-      
-            team_boards: [
-                {
-                    boardID: 3,
-                    teamID: 1,
-                    boardName: 'Project abc',
-                    date_created: new Date(),
-                    total_card: 4
-                },
-                {
-                    boardID: 4,
-                    teamID: 2,
-                    boardName: 'Project Web',
-                    date_created: new Date(),
-                    total_card: 2
-                }
-            ],
+            ],  
             layout: 'dashboard/main', title: "Dashboard", ID: 1
         });
 };
--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
 
+    $('#get-id-alert').hide();
+ 
+    $('.copy-url').on('click', function () {
+        var $temp = $("<input>");
+        console.log("checked");
+        var $url = $('.copy-url').attr('value');
+        $("body").append($temp);
+        $temp.val($url).select();
+        document.execCommand("copy");
+        $temp.remove();
+
+        $('#get-id-alert').show();
+        setTimeout( function(){$('#get-id-alert').hide(); }, 3000);
+    })
 
 });
