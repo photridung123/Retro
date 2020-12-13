8c9069d3b9974519a1ce0b5aa20ed233740428ed
photridung123 <55821539+photridung123@users.noreply.github.com>
2020-12-13 13:21:23 +0700

    + add source page board

---
+++ controllers/boardController.js
@@ -0,0 +1,59 @@
+exports.index = (req, res, next) => {
+    // Get from model
+
+    // Pass data to view to display
+    res.render('board',
+        {
+            board_name: "Project CNPM",
+            max_vote: 6,
+            columns:
+                [
+                    {
+                        column_name: "Todo",
+                        task:
+                            [
+                                {
+                                    task_name: "Xây dựng website",
+                                    task_owner: "Nguyễn Văn A",
+                                    total_vote: 0,
+                                    total_comment: 0
+                                }
+
+                            ],
+                    },
+                    {
+                        column_name: "Doing",
+                        task:
+                            [
+                                {
+                                    task_name: "Xây dựng prototype website",
+                                    task_owner: "Trần Thị B",
+                                    total_vote: 0,
+                                    total_comment: 0
+                                }
+
+                            ],
+                    },
+                    {
+                        column_name: "Done",
+                        task:
+                            [
+                                {
+                                    task_name: "Thiết kế use case",
+                                    task_owner: "Phạm Văn C",
+                                    total_vote: 0,
+                                    total_comment: 1,
+                                    comment: [
+                                        {
+                                            comment_owner: "Phạm Văn C",
+                                            comment_text: "Mọi người check với"
+                                        }
+                                    ]
+                                }
+
+                            ],
+                    },
+                ],
+            layout: 'dashboard/main', title: "Board", ID: 1
+        });
+};
