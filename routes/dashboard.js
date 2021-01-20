ab1183cc60f61f5dfa8de5a9ea4d84ea5b80847d
photridung123 <55821539+photridung123@users.noreply.github.com>
2021-01-20 16:05:58 +0700

    + add check amount board in user pricing

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,6 +4,7 @@ const dashboardController = require('../controllers/dashboardController');
 
 /* GET home page. */
 router.get('/',  dashboardController.index);
+router.post('/current-board',  dashboardController.getCurrentBoard);
 router.post('/create-board',  dashboardController.AddBoard);
 router.post('/delete-board',  dashboardController.DeleteBoard);
 module.exports = router;
